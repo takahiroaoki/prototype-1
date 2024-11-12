917c962f2c9162ce37ab87e366dabd02177b883e
takahiroaoki <69064981+takahiroaoki@users.noreply.github.com>
2024-11-12 14:36:32 +0000

    view-model修正

--- frontend/src/utils/view-model.ts
+++ frontend/src/utils/view-model.ts
@@ -1,9 +1,11 @@
 export class ViewModel {
-    private elem: HTMLElement
+    protected elem: HTMLElement
+    protected ref: string
     private emitter = new EventTarget()
 
     constructor(elem: HTMLElement) {
         this.elem = elem
+        this.ref = elem.dataset.ref ?? ''
     }
 
     protected select(selector: string): HTMLElement | null {
